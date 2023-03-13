c436911a47956aa6e5a800dca0856f8728551083
Ilimbek Meimenbaev <ilimbek.meimanbaev00@gmail.com>
2023-03-14 02:20:40 +0600

    CQRS. MediatR, DI

--- Hakaton.Domain/User.cs
+++ Hakaton.Domain/User.cs
@@ -9,11 +9,11 @@ namespace Hakaton.Domain
 {
     public class User 
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-
-        public int? RoleId { get; set; }
         public Role Role { get; set; }
     }
 }
